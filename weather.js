aeac71eacb49403deff0a0a95502c9352ba1d88a
k10tad <hollywat34@gmail.com>
2026-06-29 22:13:26 +0900

    debug main

---
+++ weather.js
@@ -0,0 +1,44 @@
+//========================
+// 天気
+//========================
+
+async function loadWeather() {
+    const temperature = document.getElementById("temperature");
+    const pressure = document.getElementById("pressure");
+    const humidity = document.getElementById("humidity");
+    const WeathereatherIcon = document.getElementById("Weather-icon");
+
+    if (!temperature || !pressure || !humidity || !WeatherIcon) return;
+
+    const url =
+        "https://api.open-meteo.com/v1/forecast?latitude=34.6937&longitude=135.5023&current=temperature_2m,weather_code,surface_pressure,relative_humidity_2m";
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        temperature.textContent = data.current.temperature_2m + "℃";
+        pressure.textContent = data.current.surface_pressure + " hPa";
+        humidity.textContent = "湿度 " + data.current.relative_humidity_2m + "%";
+
+        const code = data.current.weather_code;
+        currentWeatherCode = data.current.weather_code;
+        currentPressure = data.current.surface_pressure;
+
+        let icon = "☀";
+
+        if (code >= 1 && code <= 3) icon = "⛅";
+        if (code >= 45 && code <= 48) icon = "🌫";
+        if (code >= 51 && code <= 67) icon = "🌦";
+        if (code >= 71 && code <= 77) icon = "❄";
+        if (code >= 80 && code <= 99) icon = "🌧";
+
+        weatherIcon.textContent = icon;
+
+    } catch (error) {
+        temperature.textContent = "--℃";
+        pressure.textContent = "---- hPa";
+        humidity.textContent = "取得失敗";
+        weatherIcon.textContent = "？";
+    }
+}
