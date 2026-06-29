4262a9a8786d86a35f1ddb68d35f2fb0025e980a
k10tad <hollywat34@gmail.com>
2026-06-29 21:59:38 +0900

    debug main

--- main.js
+++ main.js
@@ -2,12 +2,23 @@
 // 起動時
 //========================
 
+console.log("main.js start");
+
 updateTimer();
+console.log("timer OK");
 
 updateClock();
 setInterval(updateClock, 1000);
+console.log("clock OK");
 
 loadWeather();
+console.log("weather called");
 
 preloadImages();
+console.log("preload OK");
+
 scheduleNextBlink();
+console.log("blink called");
+
+console.log("main.js end");
+
